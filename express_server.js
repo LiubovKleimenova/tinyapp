5f008d3ace93429221bc77466d93040e768c15c4
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-17 13:13:58 -0500

    added index template

--- express_server.js
+++ express_server.js
@@ -17,8 +17,13 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/hello", (req, res) => {
