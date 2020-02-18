96406e0983d5aa75624fb64fb3714b6dcf2ef9a4
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-18 12:52:59 -0500

    added delete option

--- express_server.js
+++ express_server.js
@@ -60,10 +60,17 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
 app.get("/u/:shortURL", (req, res) => {
   let longUrl = urlDatabase[req.params.shortURL];
   res.redirect(longUrl);
 });
+
+// s
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
